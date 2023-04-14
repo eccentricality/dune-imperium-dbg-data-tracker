1bbef419b1cb5948cd2c78f824274f62945dc188
eccentricality <youngmin13@gmail.com>
2023-04-13 23:15:16 -0400

    state management for current round, player order, player decks, and app-level state

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Game.css';
 import GameSetup from '../GameSetup/GameSetup';
 import PlayerDashboard from '../PlayerDashboard/PlayerDashboard';
@@ -7,14 +7,46 @@ import Round from '../Round/Round';
 import Stats from '../Stats/Stats';
 
 const Game = () => {
+  const [currentRound, setCurrentRound] = useState(1); // Manage current round
+  const [playerOrder, setPlayerOrder] = useState([]); // Manage player order
+  const [playerDecks, setPlayerDecks] = useState([]); // Manage player decks
+  const [cardsPlayed, setCardsPlayed] = useState([]); // Manage cards played
+  const [cardsPurchased, setCardsPurchased] = useState([]); // Manage cards purchased
+  const [cardsTrashed, setCardsTrashed] = useState([]); // Manage cards trashed
+
   // Use the components from components folder
   return (
     <div className="Game">
-      <GameSetup />
-      <PlayerDashboard />
+      <GameSetup
+        setPlayerOrder={setPlayerOrder}
+        setPlayerDecks={setPlayerDecks}
+      />
+      <PlayerDashboard
+        playerOrder={playerOrder}
+        playerDecks={playerDecks}
+        cardsPlayed={cardsPlayed}
+        cardsPurchased={cardsPurchased}
+        cardsTrashed={cardsTrashed}
+      />
       <ImperiumRow />
-      <Round />
-      <Stats />
+      <Round
+        currentRound={currentRound}
+        setCurrentRound={setCurrentRound}
+        playerOrder={playerOrder}
+        playerDecks={playerDecks}
+        cardsPlayed={cardsPlayed}
+        setCardsPlayed={setCardsPlayed}
+        cardsPurchased={cardsPurchased}
+        setCardsPurchased={setCardsPurchased}
+        cardsTrashed={cardsTrashed}
+        setCardsTrashed={setCardsTrashed}
+      />
+      <Stats
+        playerOrder={playerOrder}
+        cardsPlayed={cardsPlayed}
+        cardsPurchased={cardsPurchased}
+        cardsTrashed={cardsTrashed}
+      />
     </div>
   );
 };
